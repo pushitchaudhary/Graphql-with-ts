40d4ebee7c4b751c66442ae66c12b43d9c2ddc33
Pushit Chaudhary <56717453+PushitChaudhary@users.noreply.github.com>
2024-08-27 17:58:49 +0545

    graphql apollo server setup

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,38 @@
 import express from 'express'
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+
+async function Server() {
+    
 const app = express()
 const PORT = 8000
 
+app.use(express.json())
+
+// CREATE APOLLO SERVER
+const gqlServer = new ApolloServer({
+    // Schema
+    typeDefs : `        
+        type Query {
+            hello : String
+        }
+    `,
+    resolvers : {}
+})
+
+await gqlServer.start()
+
 app.get('/',(req,res)=>{
-    res.send('server okk !!!')
+    res.send('server okk')
 })
 
+app.use('/graphql',expressMiddleware(gqlServer))
 
-app.listen(PORT,()=>{
-    console.log(`Server has started at ${PORT}`)
+
+app.listen(PORT, ()=>{
+    console.log(`Server Has Started at ${PORT}`)
 })
+
+}
+
+Server()
